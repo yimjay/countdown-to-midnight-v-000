41e0088296fab0bef55253e9a0e032db8e981b8e
Ji Yim <gsooyim@gmail.com>
2018-04-11 02:46:56 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
-#write your code here
+# counts down to 0 from a given number int
+def countdown(int)
+  while int > 0
+    puts "#{int} SECOND(S)!"
+    int -= 1 
+  end
+  "HAPPY NEW YEAR!"
+end
 
-def countdown
+# counts down to 0 from a given number int with a delay of 1s each round
+def countdown_with_sleep(int)
+  while int > 0
+    puts "#{int} SECOND(S)!"
+    int -= 1 
+    sleep 1
+  end
+  "HAPPY NEW YEAR!"
 end
